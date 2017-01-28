4b9e74a42c9f3dcd745797913817a975d064a091
franklang <lang.frank79@gmail.com>
2017-01-28 23:35:47 +0100

    [ADD] switch from .fs- to .nkd- prefix for classes + JS code optimization.

--- js/fs-pushmenu.js
+++ js/fs-pushmenu.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
-    $('a.fs-link').on('click', function(){
+    function onLinkClick(){
         // Get current nav level of clicked link
         var getLinkNavLevel = $(this)
-                                .closest('ul.fs-level')
+                                .closest('ul.nkd-level')
                                 .attr('data-level');
-        
+
         // Get clicked link sublevel state (open/closed)
         var getSubLevelState = $(this)
-                                .parent('li.fs-item')
+                                .parent('li.nkd-item')
                                 .attr('data-state');
-        
+
         // Close children sublevels
         var closeChildren = $(this)
-                                .parent('li.fs-item')
-                                .find('li.fs-item[data-state="open"]')
+                                .parent('li.nkd-item')
+                                .find('li.nkd-item[data-state="open"]')
                                 .each(function(){
                                     $(this)
                                         .attr('data-state', 'closed');
@@ -21,14 +21,14 @@ $(document).ready(function(){
 
         // Get current level
         var getCurrentLevelValue = $(this)
-                                .parent('li.fs-item')
-                                .children('ul.fs-level')
+                                .parent('li.nkd-item')
+                                .children('ul.nkd-level')
                                 .attr('data-level');
 
         // Set clicked link sublevel state (open/closed)
         function setSubLevelState(element,state){
             element
-                .parent('li.fs-item')
+                .parent('li.nkd-item')
                 .attr('data-state', state);
         }
 
@@ -36,57 +36,68 @@ $(document).ready(function(){
             setSubLevelState($(this),'open');
 
             $(this)
-                .parent('li.fs-item')
+                .parent('li.nkd-item')
                 .siblings()
-                .addClass('fs-hidden');
+                .addClass('nkd-hidden');
 
-            $('a.fs-previous')
+            $('a.nkd-previous')
                 .attr('data-current-level', + getCurrentLevelValue);
         }
-    });
+    }
 
-    $('a.fs-previous').on('click', function(){
+    function onPreviousLinkClick(){
         // Last opened
-        $('li.fs-item[data-state="open"]:last')
+        $('li.nkd-item[data-state="open"]:last')
             .attr('data-state', 'closed');
 
         // Update data-current-level value
-        var updateCurrentLevelValue = $('li.fs-item[data-state="closed"]:first')
-                        .closest('ul.fs-level')
+        var updateCurrentLevelValue = $('li.nkd-item[data-state="closed"]:first')
+                        .closest('ul.nkd-level')
                         .attr('data-level');
-        $('a.fs-previous').attr('data-current-level', updateCurrentLevelValue);
+        $('a.nkd-previous').attr('data-current-level', updateCurrentLevelValue);
 
         // Show previously hidden items again
-        $('ul.fs-level[data-level="' + updateCurrentLevelValue + '"')
-            .children('li.fs-item')
+        $('ul.nkd-level[data-level="' + updateCurrentLevelValue + '"')
+            .children('li.nkd-item')
             .siblings()
-            .removeClass('fs-hidden');
-    });
+            .removeClass('nkd-hidden');
+    }
 
-    $('a.fs-link, a.fs-previous').on('click', function(){
-        var getCurrentLevelLabel = $('li.fs-item[data-state="open"]:last')
-                                        .children('a.fs-link')
+    function showCurrentLevelLabel(){
+        var getCurrentLevelLabel = $('li.nkd-item[data-state="open"]:last')
+                                        .children('a.nkd-link')
                                         .text();
 
-        var getPreviousLevelLabel = $('li.fs-item[data-state="open"]:last')
-                                        .parent('ul.fs-level')
-                                        .prev('a.fs-link')
+        $('.nkd-current').empty().text(getCurrentLevelLabel);
+
+        if ($('.nkd-current').text().length > 0){
+            $('.nkd-current').removeClass('nkd-hidden');
+        }
+
+        else if ($('.nkd-current').text().length == 0){
+            $('.nkd-current').addClass('nkd-hidden');
+        }
+    }
+
+    function showPreviousLevelLabel(){
+        var getPreviousLevelLabel = $('li.nkd-item[data-state="open"]:last')
+                                        .parent('ul.nkd-level')
+                                        .prev('a.nkd-link')
                                         .text();
 
-        $('.fs-current').empty().text(getCurrentLevelLabel);
-        $('a.fs-previous').empty().text(getPreviousLevelLabel);
+        $('a.nkd-previous').empty().text(getPreviousLevelLabel);
 
-
-        if ($('.fs-current').text().length > 0){
-            $('.fs-current').removeClass('fs-hidden');
+        if ($('a.nkd-previous').text().length == 0){
+            $('a.nkd-previous').text('Home');
         }
+    }
 
-        else if ($('.fs-current').text().length == 0){
-            $('.fs-current').addClass('fs-hidden');
-        }
+    function showCurrentAndPreviousLevelsLabels(){
+        showCurrentLevelLabel();
+        showPreviousLevelLabel();
+    }
 
-        if ($('a.fs-previous').text().length == 0){
-            $('a.fs-previous').text('Back');
-        }        
-    });
+    $('a.nkd-link').on('click', onLinkClick);
+    $('a.nkd-previous').on('click', onPreviousLinkClick);
+    $('a.nkd-link, a.nkd-previous').on('click', showCurrentAndPreviousLevelsLabels);
 });
