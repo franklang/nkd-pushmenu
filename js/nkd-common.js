b89fbc5ea5a41e43c00e588740260908956c4d99
Frank Lang <frank.lang@netapsys.fr>
2017-02-23 15:34:24 +0100

    Extending nkdReset function for Push Menu species - almost done!

--- js/nkd-common.js
+++ js/nkd-common.js
@@ -1,15 +1,17 @@
 var nkd = {
-    // Reset DOM to its initial state
-    nkdReset: function(element){
-      element.find('[data-state="open"]').each(function(){
+  // Set clicked link sublevel state (open/closed)
+  setSubLevelState: function(element,state){
+    element
+      .parent('.js-nkd-item')
+      .attr('data-state', state);
+  }
+}
+
+;(function ($, window, undefined){
+  // Reset DOM to its initial state
+  $.fn.nkdReset = function(){
+      $(this).find('[data-state="open"]').each(function(){
         $(this).attr('data-state','closed');
       });
-    },
-
-    // Set clicked link sublevel state (open/closed)
-    setSubLevelState: function(element,state){
-      element
-        .parent('.js-nkd-item')
-        .attr('data-state', state);
-    }
-}
+  }
+}(jQuery, window));
--- js/nkd-pushmenu.js
+++ js/nkd-pushmenu.js
@@ -1,220 +1,118 @@
-// On récupère les objets communs déclarés sous le namespace 'nkd' dans nkd-common.js
-var nkdCommon = nkd;
+;(function ($, window, undefined){
 
+  var nkdPushMenuSharedVars = {};
+    nkdPushMenuSharedVars.$previous = $('.js-nkd-previous');
+    nkdPushMenuSharedVars.$currentPosition = $('.js-nkd-current-position');
+    nkdPushMenuSharedVars.$currentPositionInitialText = nkdPushMenuSharedVars.$currentPosition.text();
+    nkdPushMenuSharedVars.commonNkdReset = $.fn.nkdReset;
 
-var nkdPushMenuSharedVars = {
-  $previous: $('.js-nkd-previous'),
-  $currentPosition: $('.js-nkd-current-position'),
-  // $currentPositionInitialText: this.$currentPosition.text()
-};
+  // Reset DOM to its initial state
+  $.fn.nkdReset = function(){
+    var ret = nkdPushMenuSharedVars.commonNkdReset.apply(this, arguments);
 
-console.log(nkdCommon);
-console.log(nkdPushMenuSharedVars);
+    $(this).find('.nkd-hidden').each(function(){
+      $(this).removeClass('nkd-hidden');
+    });
+    // $(this).find('.js-nkd-previous').text(nkdPushMenuSharedVars.$currentPositionInitialText); 
+    $(this).find('.js-nkd-previous').text('toto'); 
 
-var nkdPushMenu = {
-  onLinkClick: function(){
-    // Get current nav level of clicked link
-    var getLinkNavLevel = $(this)
-      .closest('.js-nkd-level')
-      .attr('data-level');
+    return ret;
+  }
 
-    // Get clicked link sublevel state (open/closed)
-    var getSubLevelState = $(this)
-      .parent('.js-nkd-item')
-      .attr('data-state');
-
-    // Close children sublevels
-    var closeChildren = $(this)
-      .parent('.js-nkd-item')
-      .find('[data-state="open"]')
-      .each(function(){
-        $(this)
-          .attr('data-state', 'closed');
-      });
-
-    // Get current level
-    var getCurrentLevelValue = $(this)
-      .parent('.js-nkd-item')
-      .children('.js-nkd-level')
-      .attr('data-level');
-
-    if (getSubLevelState == 'closed'){
-      nkd.setSubLevelState($(this),'open');
-
-      $(this)
-        .parent('.js-nkd-item')
-        .siblings()
-        .addClass('nkd-hidden');
-
-      nkdPushMenuSharedVars.$previous
-        .attr('data-current-level', + getCurrentLevelValue);
-    }
-  },
-
-  onPreviousLinkClick: function(){
-    // Last opened
-    $('[data-state="open"]:last')
-      .attr('data-state', 'closed');
-
-    // Update data-current-level value
-    var updateCurrentLevelValue =
-      $('[data-state="closed"]:first')
-        .closest('ul.js-nkd-level')
+  $.fn.nkdPushMenu = function(){
+    function onLinkClick(){
+      // Get current nav level of clicked link
+      var getLinkNavLevel = $(this)
+        .closest('.js-nkd-level')
         .attr('data-level');
 
-    nkdPushMenuSharedVars.$previous.attr('data-current-level', updateCurrentLevelValue);
+      // Get clicked link sublevel state (open/closed)
+      var getSubLevelState = $(this)
+        .parent('.js-nkd-item')
+        .attr('data-state');
 
-    // Show previously hidden items again
-    $('[data-level="' + updateCurrentLevelValue + '"')
-      .children('.js-nkd-item')
-      .siblings()
-      .removeClass('nkd-hidden');
-  },
+      // Close children sublevels
+      var closeChildren = $(this)
+        .parent('.js-nkd-item')
+        .find('[data-state="open"]')
+        .each(function(){
+          $(this)
+            .attr('data-state', 'closed');
+        });
 
-  showCurrentLevelLabel: function(){
-    var getCurrentLevelLabel =
-      $('[data-state="open"]:last')
-        .children('.js-nkd-link')
-        .text();
+      // Get current level
+      var getCurrentLevelValue = $(this)
+        .parent('.js-nkd-item')
+        .children('.js-nkd-level')
+        .attr('data-level');
 
-    nkdPushMenuSharedVars.$currentPosition.empty().text(getCurrentLevelLabel);
+      if (getSubLevelState == 'closed'){
+        nkd.setSubLevelState($(this),'open');
 
-    if (nkdPushMenuSharedVars.$currentPosition.text().length == 0){
-      nkdPushMenuSharedVars.$currentPosition.text(nkdPushMenuSharedVars.$currentPositionInitialText);
+        $(this)
+          .parent('.js-nkd-item')
+          .siblings()
+          .addClass('nkd-hidden');
+
+        nkdPushMenuSharedVars.$previous
+          .attr('data-current-level', + getCurrentLevelValue);
+      }
     }
-  },
 
-  showPreviousLevelLabel: function(){
-    var getPreviousLevelLabel =
+    function onPreviousLinkClick(){
+      // Last opened
       $('[data-state="open"]:last')
-        .parent('.js-nkd-level')
-        .prev('.js-nkd-link')
-        .text();
+        .attr('data-state', 'closed');
 
-    nkdPushMenuSharedVars.$previous.empty().text(getPreviousLevelLabel);
+      // Update data-current-level value
+      var updateCurrentLevelValue =
+        $('[data-state="closed"]:first')
+          .closest('ul.js-nkd-level')
+          .attr('data-level');
 
-    if (nkdPushMenuSharedVars.$previous.text().length == 0){
-      nkdPushMenuSharedVars.$previous.text(nkdPushMenuSharedVars.$currentPositionInitialText);
+      nkdPushMenuSharedVars.$previous.attr('data-current-level', updateCurrentLevelValue);
+
+      // Show previously hidden items again
+      $('[data-level="' + updateCurrentLevelValue + '"')
+        .children('.js-nkd-item')
+        .siblings()
+        .removeClass('nkd-hidden');
     }
-  },
 
-  showCurrentAndPreviousLevelsLabels: function(){
-    nkdPushMenu.showCurrentLevelLabel();
-    nkdPushMenu.showPreviousLevelLabel();
-  }
-};
+    function showCurrentLevelLabel(){
+      var getCurrentLevelLabel =
+        $('[data-state="open"]:last')
+          .children('.js-nkd-link')
+          .text();
 
+      nkdPushMenuSharedVars.$currentPosition.empty().text(getCurrentLevelLabel);
 
+      if (nkdPushMenuSharedVars.$currentPosition.text().length == 0){
+        nkdPushMenuSharedVars.$currentPosition.text(nkdPushMenuSharedVars.$currentPositionInitialText);
+      }
+    }
 
-;(function ($, window, undefined){
-  $.fn.nkdPushMenu = function(){
-    $('.js-nkd-link').on('click', nkdPushMenu.onLinkClick);
-    nkdPushMenuSharedVars.$previous.on('click', nkdPushMenu.onPreviousLinkClick);
-    $('.js-nkd-link, .js-nkd-previous').on('click', nkdPushMenu.showCurrentAndPreviousLevelsLabels);
+    function showPreviousLevelLabel(){
+      var getPreviousLevelLabel =
+        $('[data-state="open"]:last')
+          .parent('.js-nkd-level')
+          .prev('.js-nkd-link')
+          .text();
+
+      nkdPushMenuSharedVars.$previous.empty().text(getPreviousLevelLabel);
+
+      if (nkdPushMenuSharedVars.$previous.text().length == 0){
+        nkdPushMenuSharedVars.$previous.text(nkdPushMenuSharedVars.$currentPositionInitialText);
+      }
+    }
+
+    function showCurrentAndPreviousLevelsLabels(){
+      showCurrentLevelLabel();
+      showPreviousLevelLabel();
+    }
+
+    $('.js-nkd-link').on('click', onLinkClick);
+    nkdPushMenuSharedVars.$previous.on('click', onPreviousLinkClick);
+    $('.js-nkd-link, .js-nkd-previous').on('click', showCurrentAndPreviousLevelsLabels);
   };
 }(jQuery, window));
-
-
-
-// ;(function ($, window, undefined){
-//   $.fn.nkdPushMenu = function(){
-//     var $previous = $('.js-nkd-previous');
-//     var $currentPosition = $('.js-nkd-current-position');
-//     var $currentPositionInitialText = $currentPosition.text();
-
-//     function onLinkClick(){
-//       // Get current nav level of clicked link
-//       var getLinkNavLevel = $(this)
-//         .closest('.js-nkd-level')
-//         .attr('data-level');
-
-//       // Get clicked link sublevel state (open/closed)
-//       var getSubLevelState = $(this)
-//         .parent('.js-nkd-item')
-//         .attr('data-state');
-
-//       // Close children sublevels
-//       var closeChildren = $(this)
-//         .parent('.js-nkd-item')
-//         .find('[data-state="open"]')
-//         .each(function(){
-//           $(this)
-//             .attr('data-state', 'closed');
-//         });
-
-//       // Get current level
-//       var getCurrentLevelValue = $(this)
-//         .parent('.js-nkd-item')
-//         .children('.js-nkd-level')
-//         .attr('data-level');
-
-//       if (getSubLevelState == 'closed'){
-//         nkd.setSubLevelState($(this),'open');
-
-//         $(this)
-//           .parent('.js-nkd-item')
-//           .siblings()
-//           .addClass('nkd-hidden');
-
-//         $previous
-//           .attr('data-current-level', + getCurrentLevelValue);
-//       }
-//     }
-
-//     function onPreviousLinkClick(){
-//       // Last opened
-//       $('[data-state="open"]:last')
-//         .attr('data-state', 'closed');
-
-//       // Update data-current-level value
-//       var updateCurrentLevelValue =
-//         $('[data-state="closed"]:first')
-//           .closest('ul.js-nkd-level')
-//           .attr('data-level');
-
-//       $previous.attr('data-current-level', updateCurrentLevelValue);
-
-//       // Show previously hidden items again
-//       $('[data-level="' + updateCurrentLevelValue + '"')
-//         .children('.js-nkd-item')
-//         .siblings()
-//         .removeClass('nkd-hidden');
-//     }
-
-//     function showCurrentLevelLabel(){
-//       var getCurrentLevelLabel =
-//         $('[data-state="open"]:last')
-//           .children('.js-nkd-link')
-//           .text();
-
-//       $currentPosition.empty().text(getCurrentLevelLabel);
-
-//       if ($currentPosition.text().length == 0){
-//         $currentPosition.text($currentPositionInitialText);
-//       }
-//     }
-
-//     function showPreviousLevelLabel(){
-//       var getPreviousLevelLabel =
-//         $('[data-state="open"]:last')
-//           .parent('.js-nkd-level')
-//           .prev('.js-nkd-link')
-//           .text();
-
-//       $previous.empty().text(getPreviousLevelLabel);
-
-//       if ($previous.text().length == 0){
-//         $previous.text($currentPositionInitialText);
-//       }
-//     }
-
-//     function showCurrentAndPreviousLevelsLabels(){
-//       showCurrentLevelLabel();
-//       showPreviousLevelLabel();
-//     }
-
-//     $('.js-nkd-link').on('click', onLinkClick);
-//     $previous.on('click', onPreviousLinkClick);
-//     $('.js-nkd-link, .js-nkd-previous').on('click', showCurrentAndPreviousLevelsLabels);
-//   };
-// }(jQuery, window));
